97a471000cf058d30383b1a828a92b99f2278da9
Etsuji Nakai <enakai@redhat.com>
2013-08-24 21:28:57 +0900

    add simple webserver manifest

--- apache.pp
+++ apache.pp
@@ -1,8 +1,27 @@
 package { 'httpd':
   ensure => latest, 
-}
+} 
 
 service { 'httpd':
   ensure => running,
-  enable => true,
+  enable => true, 
+  hasrestart => true, 
+  hasstatus => true, 
+} 
+
+file { '/var/www/html/index.html':
+  owner => 'apache',
+  group => 'apache',
+  mode => '0600',
+  content => "<h1>This is $hostname</h1>",
 }
+
+exec { 'fw-http':
+  path => '/usr/sbin',
+  command => 'lokkit -s http',
+}
+
+Package['httpd']
+  -> File['/var/www/html/index.html']
+  -> Service['httpd']
+  -> Exec['fw-http']
